e1f25f7efbea0f252b0ecf47965760a78a31c061
dev <dev@pop-os.fritz.box>
2024-11-03 22:53:38 +0100

    print screen width and height to check if import was successful

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
+from constants import *
 
 def main():
     print("Starting asteroids!")
+    print(f"screen width: {SCREEN_WIDTH} \nscreen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
