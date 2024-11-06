1a9fe2942363fa02ebd4e9489ef426ce34620fb2
dev <dev@pop-os.fritz.box>
2024-11-06 21:49:13 +0100

    feat: create game loop and quit mechanism to exit game by clicking on the x button

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+        screen.fill(0)
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
