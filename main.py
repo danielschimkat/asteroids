79c74313b73ef2b7afa88b0df6b502160e6bc315
daniel <daniel@danielschimkat.de>
2025-01-12 11:53:32 +0100

    feat: add update and draw to rotate player

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
+    player = Player(x,y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -10,7 +17,10 @@ def main():
                 exit()
 
         screen.fill(0)
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
