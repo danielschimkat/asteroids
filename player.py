7365f1d7e4695d08889ea04612c3e9bc664c0c9a
daniel <daniel@danielschimkat.de>
2025-01-19 12:18:00 +0100

    cooldown

--- player.py
+++ player.py
@@ -24,8 +24,10 @@ class Player(CircleShape):
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer-=dt
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.timer<=0:
+                self.shoot()
         if keys[pygame.K_w]:
             self.move(dt)
         if keys[pygame.K_s]:
